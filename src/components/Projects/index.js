dfbd481391e93a702f0b8e02f05257888ba14640
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-04-04 13:33:24 +0100

    Added animation to Projects h1

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
 const Projects = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
 
   return (
     <>
@@ -17,14 +23,16 @@ const Projects = () => {
               idx={15}
             />
           </h1>
-          <p className='project'>
-            <h2 className='project-title'>RESTAURANT REVIEWS</h2>
-            <p className='project-desc'>
-              A full stack web application that allows users to add reviews to restaurants 
-              in New York City, It uses the ReactJS framework for the frontend, 
-              Node.js and Express.js for the backend and MongoDB Atlas for the database. 
-              Github repository link: <br/>
-              <a href='https://github.com/Nero2005/Restaurant-Reviews'>https://github.com/Nero2005/Restaurant-Reviews</a>
+          <p className="project">
+            <h2 className="project-title">RESTAURANT REVIEWS</h2>
+            <p className="project-desc">
+              A full stack web application that allows users to add reviews to
+              restaurants in New York City, It uses the ReactJS framework for
+              the frontend, Node.js and Express.js for the backend and MongoDB
+              Atlas for the database. Github repository link: <br />
+              <a href="https://github.com/Nero2005/Restaurant-Reviews">
+                https://github.com/Nero2005/Restaurant-Reviews
+              </a>
             </p>
           </p>
         </div>
